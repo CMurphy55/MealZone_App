5a34322f7e5c0b5d752f04afab1e70d5ea11fedd
CMurphy55 <conormurphy255@gmail.com>
2021-08-24 17:49:41 +0100

    Bug Fixes and Google Map foundations
    - Menu fixes
    - Navigation graph updated to fix bug involving the user unable to navigate between fragments
    - Google play service dependancy added in gradle file
    - Manifest file updated

---
+++ app/src/main/java/ie/project/fragments/MapFragment.kt
@@ -0,0 +1,41 @@
+package ie.project.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import ie.project.R
+import kotlinx.android.synthetic.main.activity_maps.*
+import kotlinx.android.synthetic.main.fragment_map.*
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        map_view.onCreate(savedInstanceState)
+        map_view.onResume()
+
+        map_view.getMapAsync(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let{
+            googleMap = it
+        }
+    }
+
+}
